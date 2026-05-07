b21913d0f2a1578d682159dbdab3252439479941
Vanshika Solanki <vanshikasolanki1635@gmail.com>
2026-05-07 23:49:42 +0530

    Add number comparison functionality

--- lecture01/compare.py
+++ lecture01/compare.py
@@ -1,1 +1,13 @@
+# Compare two numbers
 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print(num1, "is greater than", num2)
+
+elif num2 > num1:
+    print(num2, "is greater than", num1)
+
+else:
+    print("Both numbers are equal")

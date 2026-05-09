2a3118c63dc7034ebae9588f99f25553632c15b5
Vanshika Solanki <vanshikasolanki1635@gmail.com>
2026-05-09 22:03:43 +0530

    Add grading logic based on user score input

--- lecture01/grade.py
+++ lecture01/grade.py
@@ -1,1 +1,27 @@
+score = int(input("What's your score? "))
+if score>=90:
+    print("Grade:A")
+elif score>80:
+    print("Grade:B")
+elif score>70:
+    print("Grade:C")
+elif score>60:
+    print("Grade:D")
+else:
+    print("Grade:F")
+
+OR
+
+'''score = int(input("What's your score? "))
+if score>=90 and score<=100:
+    print("Your grade is A")
+elif score>=80 and score<90:
+    print("Your grade is B")
+elif score>=70 and score<80:
+    print("Your grade is C")
+elif score>=60 and score<70:
+    print("Your grade is D")
+else:
+    print("Your grade is F") ''' 
+
 
